33c8a0d94595e28d568842cefba57e7b36b2a9ef
shubs98 <78734527+shubs98@users.noreply.github.com>
2022-11-14 09:44:15 +0100

    eerste aanpassing

--- App.tsx
+++ App.tsx
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 
-export default function App() {
+const HomeScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
+const PredictScreen = ()=> {
+  return (
+    <View style={{flex :1,justifyContent: "center", alignItems: "center"}}>
+      
+    </View>
+  )
+}
+
+const ContactusScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} options={{
+            tabBarIcon: ({color, size}) => <MaterialIcons name="home" size={size} color={color} />
+        }} />
+        <Tab.Screen name="Live Score" component={PredictScreen}  options={{
+        tabBarIcon: ({color,size}) => <FontAwesome name="product-hunt" size={size} color={color}/>
+      }} />
+        <Tab.Screen name="Contact us" component={ContactusScreen} options={{
+          tabBarIcon: ({color,size}) => <AntDesign name="contacts" size={size} color={color} />
+        }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
